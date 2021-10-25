7d20fd7bedd071783a58ed3bfa255bfd25bf9d24
Javirandu <81594335+Javirandu@users.noreply.github.com>
2021-10-25 22:35:29 +0200

    first commit to php repository

---
+++ adios.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Primer php</title>
+    </head>
+<body>
+    <!-- Comentario: Realizado por Javier Díaz González -->
+    <?php echo "Hasta luego desarrollador!!!" ?> 
+</body>
+</html>
